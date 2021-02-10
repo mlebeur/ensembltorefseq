f6a8e3b91b366c301cc3f1736948f622f2f871ad
LEBEURRIER Manuel <lebeurrier.manuel@hotmail.fr>
2021-02-10 09:37:31 +0100

    Add files via upload

---
+++ parser.py
@@ -0,0 +1,18 @@
+import os, pandas, csv, re
+import numpy as np
+from biothings.utils.dataload import dict_convert, dict_sweep
+from biothings import config
+logging = config.logger
+def load_ensembltorefseq(data_folder):
+    infile = os.path.abspath("/opt/biothings/GRCh37/ensembltorefseq/latest/download/ensembl_to_refseq.tsv")
+    assert os.path.exists(infile)
+    dat = pandas.read_csv(infile,sep="\t",squeeze=True,quoting=csv.QUOTE_NONE).to_dict(orient='records')
+    results = {}
+    for rec in dat:
+        _id = rec["Gene stable ID"]
+        process_key = lambda k: k.replace(" ","_").lower()
+		rec = dict_convert(rec,keyfn=process_key)
+		results.setdefault(_id,[]).append(rec)
+    for _id,docs in results.items():
+        doc = {"_id": _id, "ensembltorefseq" : docs}
+        yield doc
