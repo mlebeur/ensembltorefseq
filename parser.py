8b1aef41a706cf6712a6176d32d6551f2d4ecfa7
LEBEURRIER Manuel <lebeurrier.manuel@hotmail.fr>
2021-02-11 16:24:37 +0100

    Update parser.py

--- parser.py
+++ parser.py
@@ -11,8 +11,8 @@ def load_ensembltorefseq(data_folder):
     for rec in dat:
         _id = rec["Gene stable ID"]
         process_key = lambda k: k.replace(" ","_").lower()
-		rec = dict_convert(rec,keyfn=process_key)
-		results.setdefault(_id,[]).append(rec)
+	rec = dict_convert(rec,keyfn=process_key)
+	results.setdefault(_id,[]).append(rec)
     for _id,docs in results.items():
         doc = {"_id": _id, "ensembltorefseq" : docs}
         yield doc
